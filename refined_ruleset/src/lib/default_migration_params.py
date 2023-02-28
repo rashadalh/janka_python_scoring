43d22b5c6e1723857c0f5b02c58734432b7d154e
Rashad Al-Haddad <rashad@Rashads-MacBook-Pro.local>
2023-02-28 11:27:00 -0700

    adds suggested migration params

---
+++ refined_ruleset/src/lib/default_migration_params.py
@@ -0,0 +1,14 @@
+"""Reasonable default for params to start testing."""
+from lib.credit_migration_schema import MigrationParams
+
+# based on results from optimizer
+MIGRATION_PARAMS = MigrationParams(
+    c0=2.60599986,
+    xi0=69.4213835,
+    c1=4.094462218,
+    xi1=181.65380369,
+    c2=0.43581895,
+    xi2=198.9690205,
+    c3=0.43875818,
+    xi3=32.8103525
+)
